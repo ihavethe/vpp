82277dca0df4d4d2990a67af2c5480bf4eec405d
Theo Paris <theop@flowtr.dev>
2023-05-06 02:31:01 -0700

    feat: add wip compiler

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+auto main(int argc, char* argv[]) -> int {
+	std::cout << "V++ Compiler" << std::endl;
+
+	std::system("ewimagineusingabadlanguagecouldntbeyou.v");
+
+	abort();
+
+	return 69;
+}
